ff215dffd6efd756467b675263c5a8aa58a7e562
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2021-03-02 14:33:09 +0330

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Parsa Nasirimehr on 12/12/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
